9f5fdab3a72344b0920a3494cade2283d3c6820c
deveshrawat <devsrawt@gmail.com>
2021-02-09 00:40:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const { MONGODB } = require("./config");
+const resolvers = require("./graphql/resolvers");
+const typeDefs = require("./graphql/typeDefs");
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+mongoose
+   .connect(MONGODB, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+   })
+   .then(
+      server.listen({ port: 5000 }).then((res) => {
+         console.log("connected to mongodb");
+         console.log(`server is running at ${res.url}`);
+      })
+   )
+   .catch((err) => console.log(err));
---
+++ utils/validators.js
@@ -0,0 +1,37 @@
+module.exports.validateRegisterInput = (username, email, password, confirmPassword) => {
+   const errors = {};
+   if (username.trim() === "") {
+      errors.username = "username must not be empty!";
+   }
+   if (email.trim() === "") {
+      errors.email = "email must not be empty!";
+   } else {
+      const regEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!email.match(regEx)) {
+         errors.email = "Email is invalid!";
+      }
+   }
+   if (password === "") {
+      errors.password = "password must not be empty!";
+   } else if (password !== confirmPassword) {
+      errors.password = "Passwords must match!";
+   }
+   return {
+      errors: errors,
+      valid: Object.keys(errors).length < 1,
+   };
+};
+
+module.exports.validateLoginInput = (username, password) => {
+   const errors = {};
+   if (username.trim() === "") {
+      errors.username = "username must not be empty!";
+   }
+   if (password === "") {
+      errors.password = "password must not be empty!";
+   }
+   return {
+      errors: errors,
+      valid: Object.keys(errors).length < 1,
+   };
+};
